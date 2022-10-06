4cb5575109b5f7d49f5fa51018d450d216d040da
Nihaal Nz <delrius.euphoria@gmail.com>
2022-10-06 16:44:45 +0400

    Initial commit

---
+++ tkinter-bot/classes/resource.py
@@ -0,0 +1,30 @@
+import json
+
+class Resource:
+    def __init__(self):
+        with open('resources/tk_info.json') as file:
+            self.data = json.load(file)
+    
+    def get_all_wigets(self):
+        return list(self.data.keys())
+    
+    def get_all_tk_wigets(self):
+        return [wid for wid in self.get_all_wigets() if wid.startswith('tk.')]
+
+    def get_all_ttk_wigets(self):
+        return [wid for wid in self.get_all_wigets() if wid.startswith('ttk') and wid != 'ttk']
+
+    def get_data(self, wid):
+        return self.data[wid]
+
+    def get_desc(self, wid):
+        return self.data[wid]['desc']
+
+    def get_code(self, wid):
+        return self.data[wid]['code']
+
+    def get_link(self, wid):
+        return self.data[wid]['link']
+
+    def reload_resource(self):
+        self.__init__()
